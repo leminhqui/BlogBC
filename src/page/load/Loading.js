e5599d438d5a216feb63145f1ff99eb62ceb4180
lmqui <100256175+leminhqui@users.noreply.github.com>
2022-04-22 15:10:25 +0700

    Update Loading.js

--- src/page/load/Loading.js
+++ src/page/load/Loading.js
@@ -1,11 +1,41 @@
 import "./loading.css"
 import React,{Component} from 'react'
+import { toast } from "react-toastify";
+import {withRouter} from 'react-router'
 
 class Loading extends Component{
+
+
+    checkPathName(){
+        let pathname = window.location.pathname.slice(1)
+        if(this.props.authen===false)
+        {
+            switch(pathname) {
+            case "profile":
+            case "write":
+            case "mypost":
+                toast.warn("Please go to login")
+                // setTimeout(()=>{this.props.history.push("/login")},3000)
+              break;
+           
+            default:
+             break;
+          }
+        }
+        
+    }
+
+    componentDidMount(){
+        this.checkPathName()
+    }
+    componentWillUnmount(){
+        clearTimeout(this.checkPathName)
+    }
     render(){
         return (
             <>
-            <div className="loader">
+            
+            <div className="loader lr">
             <div className="outer"></div>
             <div className="middle"></div>
             <div className="inner"></div>
@@ -16,4 +46,4 @@ class Loading extends Component{
  
 }
 
-export default Loading
+export default withRouter(Loading)
