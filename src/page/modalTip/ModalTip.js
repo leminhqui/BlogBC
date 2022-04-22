0fdb89a0a8ef54c13fac159e6bcb1eed13f6e977
lmqui <100256175+leminhqui@users.noreply.github.com>
2022-04-22 15:11:06 +0700

    Update ModalTip.js

--- src/page/modalTip/ModalTip.js
+++ src/page/modalTip/ModalTip.js
@@ -1,43 +1,55 @@
-import React ,{Component} from "react";
+import React, { Component } from "react";
 import './modaltip.css'
 import Web3 from 'web3'
-class  ModalTip extends Component{
-  constructor(props){
+import { toast } from 'react-toastify'
+import Loading from "../load/Loading";
+
+
+class ModalTip extends Component {
+  constructor(props) {
     super(props)
-    this.state= {
-      tipInput:''
+    this.state = {
+      tipInput: ''
     }
   }
-  
-
-  render(){
+  tipPost(id) {
+    if (this.state.tipInput <= 0 || this.state.tipInput == null || this.state.tipInput == '') {
+      toast.error("Please enter another value !!")
+    } else {
+      let tipAmount = window.web3.utils.toWei(this.state.tipInput, 'Ether')
+      this.props.tipPost(id, tipAmount)
+    }
+  }
+  render() {
     return (
       <>
-      {this.props.isOpenModalTip
-        ? <div className="modalTipContain ">
-            <div className="modalContainer">
+        {this.props.isOpenModalTip
+          ? this.props.loading
+            ? <Loading />
+            :
+            <div className="modalTipContain ">
+              <div className="modalContainer">
                 <h1 className="modalTitle">How much would you like to tip?</h1>
                 <input
-                    onChange={event=> this.setState({tipInput: event.target.value})}
-                    id="modalTip" type="text" placeholder="0.01 ETH" className="tipInput" 
-                    autoFocus={true} required/>
-                <div className="modalButtonContain"> 
-                <button className="tipSend"
-                  onClick={(event)=>{
-                      let tipAmount = window.web3.utils.toWei(this.state.tipInput, 'Ether')
-                      this.props.tipPoste(this.props.id, tipAmount)
-                  }} 
-                
-                >OK</button>
-                <button className="tipCancel" onClick={()=>{this.props.handleCloseModalTip()}}>Cancel</button>
+                  onChange={event => this.setState({ tipInput: event.target.value })}
+                  id="modalTip" type="number" placeholder="0.01 ETH" className="tipInput"
+                  autoFocus={true} required />
+                <div className="modalButtonContain">
+                  <button className="tipSend"
+                    onClick={(event) => {
+                      this.tipPost(this.props.id)
+                    }}
+
+                  >OK</button>
+                  <button className="tipCancel" onClick={() => { this.props.handleCloseModalTip() }}>Cancel</button>
                 </div>
+              </div>
             </div>
-          </div>
-        :<></>
-      }
-      
+          : <></>
+        }
+
       </>
     )
- }
+  }
 }
 export default ModalTip
